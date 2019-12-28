2991332a79cf6bd0809f12e7f1daae4a254f52b6
juankasem <juankasem982@gmail.com>
2019-12-28 16:23:13 +0300

    Initialize Ad product Modal form

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -2,7 +2,7 @@ import { ProductService } from './../../services/product.service';
 import { Observable, from } from 'rxjs';
 import { Product } from './../../interfaces/product';
 import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 import { Subject } from 'rxjs';
 import { DataTableDirective } from 'angular-datatables';
@@ -36,7 +36,6 @@ export class ProductListComponent implements OnInit {
    //Update Modal
    @ViewChild('editTemplate',{static : false}) editModal : TemplateRef<any>;
   
-
    //Modal Properties
    modalMessage : string;
    modalRef : BsModalRef;
@@ -50,7 +49,9 @@ export class ProductListComponent implements OnInit {
    dtTrigger : Subject<any> = new Subject();
 
    @ViewChild(DataTableDirective, {static : false}) dtElement : DataTableDirective;
-  constructor(private productService : ProductService) { }
+  constructor(private productService : ProductService, 
+              private modalService : BsModalService,
+              private fb : FormBuilder) { }
 
   ngOnInit() {
     this.dtOptions = {
@@ -64,6 +65,30 @@ export class ProductListComponent implements OnInit {
       this.products$.subscribe(res => {
        this.products = res;
        this.dtTrigger.next()
-     })
+     });
+    
+   //Modal message
+   this.modalMessage = "All fields are mandatory";
+
+   //Initialize Add Product properties
+    let validateImageUrl : string = '^(https?:\/\/.*\.(?:png|jpg))$';
+
+    this.name = new FormControl('', [Validators.required, Validators.maxLength(50)]);
+    this.description = new FormControl('', [Validators.required, Validators.min(0), Validators.max(10000)]);
+    this.price = new FormControl('', [Validators.required, Validators.maxLength(150)]);
+    this.imageUrl = new FormControl('', Validators.pattern(validateImageUrl));
+     
+    this.insertForm = this.fb.group({
+      'name' : this.name,
+      'description' : this.description,
+      'price' : this.price,
+      'imageUrl' : this.imageUrl,
+      'outOfStock' : true
+    })
+  }
+
+  onAddProduct()
+  {
+    this.modalRef = this.modalService.show(this.modal);  
   }
 }
